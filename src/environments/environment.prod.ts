2963e40ee17d532c4dab4459b9a9a0082fd8b0ab
Chris <capozzic1@gmail.com>
2025-12-10 13:27:18 -0500

    update readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // Set this to your production API host
-  apiBase: 'http://hms-alb-2110211123.us-east-2.elb.amazonaws.com/api'
+  apiBase: 'http://hmsv2.us-east-1.elasticbeanstalk.com/api'
 };
